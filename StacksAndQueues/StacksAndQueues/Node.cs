15516f3bfe291ed70d6a30a1639186118bcd3d1e
ravi04ez <ezekie.em@gmail.com>
2022-07-31 14:09:07 +0530

    Wlcm Msg Stacks & Queues

---
+++ StacksAndQueues/StacksAndQueues/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class Node
+    {
+        public int Value;
+        public Node Next;
+
+        public Node(int value)
+        {
+            this.Value = value;
+            
+        }
+    }
+}
---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t#######################################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO STACKS AND QUEUES PROGRAM");
+            Console.WriteLine("\t\t\t\t#######################################################");
+            Console.ReadKey();
+
+        }
+    }
+}
